574218aab2fc7f7cc55795ec43875cd1a12d93b6
Lea SANCHEZ <lesanche@student.42.fr>
2019-02-17 16:47:09 +0100

    add

--- build/LouiseWeiss.js
+++ build/LouiseWeiss.js
@@ -1,4 +1,3 @@
-"use strict";
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
@@ -46,12 +45,25 @@ var ChooseCharacter = /** @class */ (function (_super) {
 var Pacman = /** @class */ (function (_super) {
     __extends(Pacman, _super);
     function Pacman() {
-        return _super.call(this, { key: 'Pacman', active: false }) || this;
+        var _this = _super.call(this, { key: 'Pacman', active: false }) || this;
+        // Directions
+        _this.NONE = 0;
+        _this.LEFT = 1;
+        _this.RIGHT = 2;
+        _this.UP = 3;
+        _this.DOWN = 4;
+        _this.PLAYER_SPEED = 60;
+        _this.STAR_NB = 4;
+        _this.TurnPoint = new Phaser.Math.Vector2();
+        _this.Directions = [null, null, null, null, null];
+        _this.Opposites = [_this.NONE, _this.RIGHT, _this.LEFT, _this.DOWN, _this.UP];
+        return _this;
         // this.Player = new Physics.Arcade.Sprite();
     }
     Pacman.prototype.preload = function () {
         this.load.spritesheet("arcade", "assets/Clara.png", { frameWidth: 64, frameHeight: 64 });
         this.load.image('mapTiles', 'assets/PacmanMap.png');
+        this.load.image('star', 'assets/star.png');
     };
     Pacman.prototype.create = function () {
         var _this = this;
@@ -72,15 +84,15 @@ var Pacman = /** @class */ (function (_super) {
             [16, 17, 17, 17, 17, 17, 17, 17, 17, 18],
             [32, 33, 33, 33, 33, 33, 33, 33, 33, 34]
         ];
-        var map = this.make.tilemap({ data: level, tileWidth: 32, tileHeight: 32 });
-        var tiles = map.addTilesetImage('mapTiles');
-        var layer = map.createDynamicLayer(0, tiles, 0, 0);
+        this.TileMap = this.make.tilemap({ data: level, tileWidth: 32, tileHeight: 32 });
+        var tiles = this.TileMap.addTilesetImage('mapTiles');
+        var layer = this.TileMap.createDynamicLayer(0, tiles, 0, 0);
         layer.setCollisionBetween(0, 2);
         layer.setCollision(16);
         layer.setCollision(18);
         layer.setCollisionBetween(32, 34);
-        var widthRatio = this.sys.canvas.width / (map.tileWidth * map.width);
-        var heightRatio = this.sys.canvas.height / (map.tileHeight * map.height);
+        var widthRatio = this.sys.canvas.width / (this.TileMap.tileWidth * this.TileMap.width);
+        var heightRatio = this.sys.canvas.height / (this.TileMap.tileHeight * this.TileMap.height);
         this.ScaleRatio = widthRatio > heightRatio ? heightRatio : widthRatio;
         layer.setScale(this.ScaleRatio, this.ScaleRatio);
         var debugGraphics = this.add.graphics().setAlpha(0.75);
@@ -89,12 +101,12 @@ var Pacman = /** @class */ (function (_super) {
             collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
             faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
         });
-        console.log(this);
-        this.Player = this.physics.add.sprite(100, 100, "arcade");
-        this.Player.setScale(this.ScaleRatio * 0.5, this.ScaleRatio * 0.5);
-        // this.Player.setCollideWorldBounds(true);
-        var test = this.physics.add.collider(this.Player, layer);
-        // test.collideCallback()
+        this.Player = this.physics.add.sprite(48 * this.ScaleRatio, 48 * this.ScaleRatio, "arcade");
+        this.Player.setScale(this.ScaleRatio * 0.4, this.ScaleRatio * 0.4);
+        console.log(32 * this.ScaleRatio + " - " + this.Player.displayWidth + " / 2");
+        this.Threshold = Math.ceil((32 * this.ScaleRatio - this.Player.displayWidth) / 2);
+        console.log(this.Threshold);
+        this.physics.add.collider(this.Player, layer);
         this.Cursors = this.input.keyboard.createCursorKeys();
         this.anims.create({
             key: "right",
@@ -120,7 +132,13 @@ var Pacman = /** @class */ (function (_super) {
             frameRate: 10,
             repeat: -1
         });
-        var downX, upX, downY, upY, threshold = 50;
+        this.Stars = this.physics.add.group();
+        this.Stars.create(this.gridToWorld(2), this.gridToWorld(7), 'star');
+        this.Stars.create(this.gridToWorld(6), this.gridToWorld(1), 'star');
+        this.Stars.create(this.gridToWorld(8), this.gridToWorld(8), 'star');
+        this.Stars.create(this.gridToWorld(1), this.gridToWorld(12), 'star');
+        this.physics.add.overlap(this.Player, this.Stars, this.collectStar, null, this);
+        var downX, upX, downY, upY, Threshold = 50;
         this.input.on('pointerdown', function (pointer) {
             downX = pointer.x;
             downY = pointer.y;
@@ -128,43 +146,135 @@ var Pacman = /** @class */ (function (_super) {
         this.input.on('pointerup', function (pointer) {
             upX = pointer.x;
             upY = pointer.y;
-            if (upX < downX - threshold) {
+            if (upX < downX - Threshold) {
                 _this.Swipe = 'left';
             }
-            else if (upX > downX + threshold) {
+            else if (upX > downX + Threshold) {
                 _this.Swipe = 'right';
             }
-            else if (upY < downY - threshold) {
+            else if (upY < downY - Threshold) {
                 _this.Swipe = 'up';
             }
-            else if (upY > downY + threshold) {
+            else if (upY > downY + Threshold) {
                 _this.Swipe = 'down';
             }
         });
+        this.move(this.RIGHT);
     };
     Pacman.prototype.update = function () {
+        this.Marker = new Phaser.Math.Vector2();
+        // this.Marker.x = Phaser.Math.Snap.Floor(Math.floor(this.Player.x), 32) / 32;
+        // this.Marker.y = Phaser.Math.Snap.Floor(Math.floor(this.Player.y), 32) / 32;
+        var playerTile = this.TileMap.getTileAtWorldXY(this.Player.x, this.Player.y);
+        this.Marker.x = playerTile.x;
+        this.Marker.y = playerTile.y;
+        this.Directions[this.LEFT] = this.TileMap.getTileAt(playerTile.x - 1, playerTile.y);
+        this.Directions[this.RIGHT] = this.TileMap.getTileAt(playerTile.x + 1, playerTile.y);
+        this.Directions[this.UP] = this.TileMap.getTileAt(playerTile.x, playerTile.y - 1);
+        this.Directions[this.DOWN] = this.TileMap.getTileAt(playerTile.x, playerTile.y + 1);
         if (this.Cursors.right != undefined && this.Cursors.right.isDown || this.Swipe == 'right') {
-            this.Player.setVelocityX(+60 * this.ScaleRatio);
-            // this.Player.setVelocityY(0);
-            this.Player.anims.play('right', true);
+            this.checkDirection(this.RIGHT);
         }
         else if (this.Cursors.left != undefined && this.Cursors.left.isDown || this.Swipe == 'left') {
-            this.Player.setVelocityX(-60 * this.ScaleRatio);
-            // this.Player.setVelocityY(0);
-            this.Player.anims.play('left', true);
+            this.checkDirection(this.LEFT);
         }
         else if (this.Cursors.up != undefined && this.Cursors.up.isDown || this.Swipe == 'up') {
-            this.Player.setVelocityY(-60 * this.ScaleRatio);
-            // this.Player.setVelocityX(0);
-            this.Player.anims.play('up', true);
+            this.checkDirection(this.UP);
         }
         else if (this.Cursors.down != undefined && this.Cursors.down.isDown || this.Swipe == 'down') {
-            this.Player.setVelocityY(+60 * this.ScaleRatio);
-            // this.Player.setVelocityX(0);
-            this.Player.anims.play('down', true);
+            this.checkDirection(this.DOWN);
         }
         this.Swipe = '';
-        // console.log(this.Cursors)
+        if (this.Turning != this.Current && this.Turning !== this.NONE) {
+            this.turn();
+        }
+    };
+    Pacman.prototype.move = function (direction) {
+        if (direction === this.LEFT) {
+            this.Player.setVelocityX(-this.PLAYER_SPEED * this.ScaleRatio);
+            if (this.Player.anims.currentAnim !== this.anims.get('left')) {
+                this.Player.anims.play('left', true);
+            }
+        }
+        else if (direction === this.RIGHT) {
+            this.Player.setVelocityX(this.PLAYER_SPEED * this.ScaleRatio);
+            if (this.Player.anims.currentAnim !== this.anims.get('right')) {
+                this.Player.anims.play('right', true);
+            }
+        }
+        else if (direction === this.UP) {
+            this.Player.setVelocityY(-this.PLAYER_SPEED * this.ScaleRatio);
+            if (this.Player.anims.currentAnim !== this.anims.get('up')) {
+                this.Player.anims.play('up', true);
+            }
+        }
+        else {
+            this.Player.setVelocityY(this.PLAYER_SPEED * this.ScaleRatio);
+            if (this.Player.anims.currentAnim !== this.anims.get('down')) {
+                this.Player.anims.play('down', true);
+            }
+        }
+        this.Current = direction;
+    };
+    Pacman.prototype.turn = function () {
+        var cx = Math.floor(this.Player.x);
+        var cy = Math.floor(this.Player.y);
+        // var cx = this.Player.x;
+        // var cy = this.Player.y;
+        if (this.isHorizontalAxis(this.Current)) {
+            if (this.Current == this.RIGHT && cx > this.TurnPoint.x + this.Threshold) {
+                this.Turning = this.NONE;
+                return false;
+            }
+            else if (cx < this.TurnPoint.x - this.Threshold) {
+                this.Turning = this.NONE;
+                return false;
+            }
+        }
+        else {
+            if (this.Current == this.DOWN && cy > this.TurnPoint.y + this.Threshold) {
+                this.Turning = this.NONE;
+                return false;
+            }
+            else if (cy < this.TurnPoint.y - this.Threshold) {
+                console.log(cy + " < " + this.TurnPoint.y + " - " + this.Threshold);
+                this.Turning = this.NONE;
+                return false;
+            }
+        }
+        // console.log(`${cy} == ${this.TurnPoint.y} | ${cx} == ${this.TurnPoint.x}`);
+        if (!Phaser.Math.Fuzzy.Equal(cx, this.TurnPoint.x, this.Threshold)
+            || !Phaser.Math.Fuzzy.Equal(cy, this.TurnPoint.y, this.Threshold)) {
+            return false;
+        }
+        this.Player.x = this.TurnPoint.x;
+        this.Player.y = this.TurnPoint.y;
+        this.Player.body.reset(this.TurnPoint.x, this.TurnPoint.y);
+        this.move(this.Turning);
+        this.Turning = this.NONE;
+        return true;
+    };
+    Pacman.prototype.isHorizontalAxis = function (direction) {
+        return (direction == this.LEFT || direction == this.RIGHT);
+    };
+    Pacman.prototype.checkDirection = function (direction) {
+        if (this.Turning === direction || this.Directions[direction] === null || this.Directions[direction].index != 17) {
+            return;
+        }
+        if (this.Current === this.Opposites[direction]) {
+            this.move(direction);
+        }
+        else {
+            this.Turning = direction;
+            this.TurnPoint.x = (this.Marker.x * 32 + 16) * this.ScaleRatio;
+            this.TurnPoint.y = (this.Marker.y * 32 + 16) * this.ScaleRatio;
+        }
+    };
+    Pacman.prototype.gridToWorld = function (pos) {
+        return ((pos * 32 + 16) * this.ScaleRatio);
+    };
+    Pacman.prototype.collectStar = function (player, star) {
+        star.disableBody(true, true);
     };
     return Pacman;
 }(Phaser.Scene));
--- src/PacmanScene.ts
+++ src/PacmanScene.ts
@@ -1,8 +1,34 @@
+
 class Pacman extends Phaser.Scene {
+
+    // Directions
+    NONE = 0;
+    LEFT = 1;
+    RIGHT = 2;
+    UP = 3;
+    DOWN = 4;
+
+    PLAYER_SPEED = 60;
+
+    STAR_NB = 4;
+
+    ScaleRatio!: number;
+    TileMap!: Phaser.Tilemaps.Tilemap;
     Player!: Phaser.Physics.Arcade.Sprite;
+    Stars!: Phaser.Physics.Arcade.Group;
+
+
+    // Player movement
     Cursors!: Phaser.Input.Keyboard.CursorKeys;
     Swipe!: string;
-    ScaleRatio!: number;
+    Threshold!: number;
+    Marker!: Phaser.Math. Vector2;
+    Current!: number;
+    Turning!: number;
+
+    TurnPoint = new Phaser.Math.Vector2();
+    Directions: Array<Phaser.Tilemaps.Tile> = [ null, null, null, null, null ];
+    Opposites: Array<number> = [ this.NONE, this.RIGHT, this.LEFT, this.DOWN, this.UP ];
 
     constructor() {
         super({ key: 'Pacman', active:false });
@@ -12,6 +38,7 @@ class Pacman extends Phaser.Scene {
     preload(){
         this.load.spritesheet("arcade", "assets/Clara.png", { frameWidth:64, frameHeight:64});
         this.load.image('mapTiles', 'assets/PacmanMap.png');
+        this.load.image('star', 'assets/star.png');
     }
 
     create() {
@@ -33,15 +60,15 @@ class Pacman extends Phaser.Scene {
             [32, 33, 33, 33, 33, 33, 33, 33, 33, 34]
         ];
 
-        var map = this.make.tilemap({ data: level, tileWidth: 32, tileHeight: 32 });
-        var tiles = map.addTilesetImage('mapTiles');
-        var layer = map.createDynamicLayer(0, tiles, 0, 0);
+        this.TileMap = this.make.tilemap({ data: level, tileWidth: 32, tileHeight: 32 });
+        var tiles = this.TileMap.addTilesetImage('mapTiles');
+        var layer = this.TileMap.createDynamicLayer(0, tiles, 0, 0);
         layer.setCollisionBetween(0, 2);
         layer.setCollision(16);
         layer.setCollision(18);
         layer.setCollisionBetween(32, 34);
-        var widthRatio = this.sys.canvas.width / (map.tileWidth * map.width);
-        var heightRatio = this.sys.canvas.height / (map.tileHeight * map.height);
+        var widthRatio = this.sys.canvas.width / (this.TileMap.tileWidth * this.TileMap.width);
+        var heightRatio = this.sys.canvas.height / (this.TileMap.tileHeight * this.TileMap.height);
         this.ScaleRatio = widthRatio > heightRatio ? heightRatio : widthRatio;
         layer.setScale(this.ScaleRatio, this.ScaleRatio);
 
@@ -52,13 +79,15 @@ class Pacman extends Phaser.Scene {
           faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
         });
 
-        console.log(this);
-        this.Player = this.physics.add.sprite(100,100,"arcade");
-        this.Player.setScale(this.ScaleRatio * 0.5, this.ScaleRatio * 0.5);
-        // this.Player.setCollideWorldBounds(true);
-        var test = this.physics.add.collider(this.Player, layer);
-        // test.collideCallback()
+        this.Player = this.physics.add.sprite(48 * this.ScaleRatio,48 * this.ScaleRatio,"arcade");
+        this.Player.setScale(this.ScaleRatio * 0.4, this.ScaleRatio * 0.4);
+        console.log(`${32 * this.ScaleRatio} - ${this.Player.displayWidth} / 2`);
+        this.Threshold = Math.ceil((32 * this.ScaleRatio - this.Player.displayWidth) / 2);
+        console.log(this.Threshold);
+        this.physics.add.collider(this.Player, layer);
+
         this.Cursors = this.input.keyboard.createCursorKeys();
+
         this.anims.create({
             key:"right",
             frames:this.anims.generateFrameNumbers('arcade', { start: 1, end:6 }),
@@ -84,7 +113,16 @@ class Pacman extends Phaser.Scene {
             repeat: -1
         });
 
-        var downX: number, upX: number, downY: number, upY: number, threshold: number = 50;
+        this.Stars = this.physics.add.group();
+
+        this.Stars.create(this.gridToWorld(2), this.gridToWorld(7), 'star');
+        this.Stars.create(this.gridToWorld(6), this.gridToWorld(1), 'star');
+        this.Stars.create(this.gridToWorld(8), this.gridToWorld(8), 'star');
+        this.Stars.create(this.gridToWorld(1), this.gridToWorld(12), 'star');
+
+        this.physics.add.overlap(this.Player, this.Stars, this.collectStar, null, this);
+
+        var downX: number, upX: number, downY: number, upY: number, Threshold: number = 50;
         this.input.on('pointerdown', function (pointer : Phaser.Input.InputPlugin) {
             downX = pointer.x;
             downY = pointer.y;
@@ -92,42 +130,147 @@ class Pacman extends Phaser.Scene {
         this.input.on('pointerup', (pointer : Phaser.Input.InputPlugin) => {
             upX = pointer.x;
             upY = pointer.y;
-            if (upX < downX - threshold){
+            if (upX < downX - Threshold){
                 this.Swipe = 'left';
-            } else if (upX > downX + threshold) {
+            } else if (upX > downX + Threshold) {
                 this.Swipe = 'right';
-            } else if (upY < downY - threshold) {
+            } else if (upY < downY - Threshold) {
                 this.Swipe = 'up';
-            } else if (upY > downY + threshold) {
+            } else if (upY > downY + Threshold) {
                 this.Swipe = 'down';
             }
         });     
 
+        this.move(this.RIGHT);
     }
 
     update() {
+        this.Marker = new Phaser.Math.Vector2();
+        // this.Marker.x = Phaser.Math.Snap.Floor(Math.floor(this.Player.x), 32) / 32;
+        // this.Marker.y = Phaser.Math.Snap.Floor(Math.floor(this.Player.y), 32) / 32;
+
+        var playerTile = this.TileMap.getTileAtWorldXY(this.Player.x, this.Player.y);
+        this.Marker.x = playerTile.x;
+        this.Marker.y = playerTile.y;
+        this.Directions[this.LEFT] = this.TileMap.getTileAt(playerTile.x - 1, playerTile.y);
+        this.Directions[this.RIGHT] = this.TileMap.getTileAt(playerTile.x + 1, playerTile.y);
+        this.Directions[this.UP] = this.TileMap.getTileAt(playerTile.x, playerTile.y - 1);
+        this.Directions[this.DOWN] = this.TileMap.getTileAt(playerTile.x, playerTile.y + 1);
+
         if (this.Cursors.right != undefined && this.Cursors.right.isDown || this.Swipe == 'right'){
-            this.Player.setVelocityX(+60 * this.ScaleRatio);
-            // this.Player.setVelocityY(0);
-            this.Player.anims.play('right', true);
+            this.checkDirection(this.RIGHT);
         }
         else if (this.Cursors.left != undefined && this.Cursors.left.isDown || this.Swipe == 'left'){
-            this.Player.setVelocityX(-60 * this.ScaleRatio);
-            // this.Player.setVelocityY(0);
-            this.Player.anims.play('left', true);
+            this.checkDirection(this.LEFT);
+
         }
         else if (this.Cursors.up != undefined && this.Cursors.up.isDown || this.Swipe == 'up'){
-            this.Player.setVelocityY(-60 * this.ScaleRatio);
-            // this.Player.setVelocityX(0);
-            this.Player.anims.play('up', true);
+            this.checkDirection(this.UP);
+
         }
         else if (this.Cursors.down != undefined && this.Cursors.down.isDown || this.Swipe == 'down'){
-            this.Player.setVelocityY(+60 * this.ScaleRatio);
-            // this.Player.setVelocityX(0);
-            this.Player.anims.play('down', true);
+            this.checkDirection(this.DOWN);
         }
         this.Swipe = '';
-        // console.log(this.Cursors)
+
+        if (this.Turning != this.Current && this.Turning !== this.NONE) {
+            this.turn();
+        }
     }
 
+    move(direction: number)
+    {
+        if (direction === this.LEFT) {
+            this.Player.setVelocityX(-this.PLAYER_SPEED * this.ScaleRatio);
+            if (this.Player.anims.currentAnim !== this.anims.get('left')) {
+                this.Player.anims.play('left', true);
+            }
+        } else if (direction === this.RIGHT) {
+            this.Player.setVelocityX(this.PLAYER_SPEED * this.ScaleRatio);
+            if (this.Player.anims.currentAnim !== this.anims.get('right')) {
+                this.Player.anims.play('right', true);
+            }
+        } else if (direction === this.UP) {
+            this.Player.setVelocityY(-this.PLAYER_SPEED * this.ScaleRatio);
+            if (this.Player.anims.currentAnim !== this.anims.get('up')) {
+                this.Player.anims.play('up', true);
+            }
+        } else {
+            this.Player.setVelocityY(this.PLAYER_SPEED * this.ScaleRatio);
+            if (this.Player.anims.currentAnim !== this.anims.get('down')) {
+                this.Player.anims.play('down', true);
+            }
+        }
+
+        this.Current = direction;
+    }
+
+    turn()
+    {
+        var cx = Math.floor(this.Player.x);
+        var cy = Math.floor(this.Player.y);
+        // var cx = this.Player.x;
+        // var cy = this.Player.y;
+
+        if (this.isHorizontalAxis(this.Current)) {
+            if (this.Current == this.RIGHT && cx > this.TurnPoint.x + this.Threshold) {
+                this.Turning = this.NONE;
+                return false;
+            } else if (cx < this.TurnPoint.x - this.Threshold) {
+                this.Turning = this.NONE;
+                return false;
+            }
+        } else {
+            if (this.Current == this.DOWN && cy > this.TurnPoint.y + this.Threshold) {
+                this.Turning = this.NONE;
+                return false;
+            } else if (cy < this.TurnPoint.y - this.Threshold) {
+                console.log(`${cy} < ${this.TurnPoint.y} - ${this.Threshold}`);
+                this.Turning = this.NONE;
+                return false;
+            }
+        }
+        // console.log(`${cy} == ${this.TurnPoint.y} | ${cx} == ${this.TurnPoint.x}`);
+        if (!Phaser.Math.Fuzzy.Equal(cx, this.TurnPoint.x, this.Threshold)
+            || !Phaser.Math.Fuzzy.Equal(cy, this.TurnPoint.y, this.Threshold)) {
+            return false;
+        }
+
+        this.Player.x = this.TurnPoint.x;
+        this.Player.y = this.TurnPoint.y;
+
+        this.Player.body.reset(this.TurnPoint.x, this.TurnPoint.y);
+        this.move(this.Turning);
+        this.Turning = this.NONE;
+
+        return true;
+    }
+
+    isHorizontalAxis(direction: number)
+    {
+        return (direction == this.LEFT || direction == this.RIGHT);
+    }
+
+    checkDirection(direction: number) {
+        if (this.Turning === direction || this.Directions[direction] === null || this.Directions[direction].index != 17) {
+            return;
+        }
+
+        if (this.Current === this.Opposites[direction]) {
+            this.move(direction);
+        } else {
+            this.Turning = direction;
+
+            this.TurnPoint.x = (this.Marker.x * 32 + 16) * this.ScaleRatio;
+            this.TurnPoint.y = (this.Marker.y * 32 + 16) * this.ScaleRatio;
+        }
+    }
+
+    gridToWorld(pos: number) {
+        return ((pos * 32 + 16) * this.ScaleRatio);
+    }
+
+    collectStar(player: Phaser.Physics.Arcade.Sprite, star: Phaser.Physics.Arcade.Sprite) {
+        star.disableBody(true, true);
+    }
 }
