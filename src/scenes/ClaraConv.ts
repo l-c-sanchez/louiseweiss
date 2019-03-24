32d189607677de64cb098161b9e14cd18ef1ea83
Léa Sanchez <sanchezlea.media@gmail.com>
2019-03-24 15:42:03 +0100

    adding instructions to scene conv

--- src/scenes/ClaraConv.ts
+++ src/scenes/ClaraConv.ts
@@ -1,12 +1,14 @@
 import { Config } from "../Config";
 import { HudScene } from "./HudScene";
 import { DialogTree } from "../utils/DialogTree";
-import { Anchor, DialogOptions, ButtonOptions } from "../utils/DialogBox";
+import {  DialogBox, Anchor, DialogOptions, ButtonOptions } from "../utils/DialogBox";
 import { DialogPhone } from "../utils/DialogPhone";
 
 export class ClaraConv extends Phaser.Scene {
-	private Hud		: HudScene;
-	private Dialogs	: DialogPhone;
+	private Hud			 : HudScene;
+	private StartDialog	 : DialogBox = null;
+	private Dialogs		 : DialogPhone;
+	private Config       : any;
 	// private Dialogs	: DialogTree;
 
     constructor() {
@@ -22,9 +24,15 @@ export class ClaraConv extends Phaser.Scene {
 	}
 
 	create() {
-		let dialogContent = this.cache.json.get('ClaraConv');
-        this.cameras.main.setBackgroundColor("#ffffff");
+		var character: string = this.registry.get('character');
+        var games = this.cache.json.get('Games');
+
+		this.Config = games.Conv[character];
 		
+		let dialogContent = this.cache.json.get('ClaraConv');
+        this.cameras.main.setBackgroundColor("#ffffff"); 
+        this.StartDialog = new DialogBox(this, this.Config.instruction, false, Anchor.Center, { windowHeight: 410, fontSize: 22 });
+        
 		let messageOptions: DialogOptions = {
 			fitContent: true,
 			windowColor: 0xedecec,
@@ -54,6 +62,8 @@ export class ClaraConv extends Phaser.Scene {
 
 		this.Dialogs = new DialogPhone(this, dialogContent, false, messageOptions, answerOptions,
 			inputFieldOptions, buttonOptions);
+
+		
 
 		this.add.existing(this.Dialogs);
 		this.Dialogs.on('destroy', () => {
--- src/scenes/QuizzClara.ts
+++ src/scenes/QuizzClara.ts
@@ -1,5 +1,6 @@
 import { Config } from "../Config";
 import { HudScene } from "./HudScene";
+import { GameText } from "../utils/GameText";
 import { DialogTree } from "../utils/DialogTree";
 import { DialogBox, Anchor, DialogOptions, ButtonOptions } from "../utils/DialogBox";
 import { DialogPhone } from "../utils/DialogPhone";
@@ -15,9 +16,11 @@ export class QuizzClara extends Phaser.Scene {
     private Dialogs	: DialogTree;
     TextData	 : any;
     StartDialog	 : DialogBox = null;
-    GameEnded    : boolean;
+    GameEnded    : boolean = false;
     GameState    : State;
     Config       : any;
+    TextDuringFlash : GameText
+    Button : Phaser.GameObjects.Sprite
 	// private Dialogs	: DialogTree;
 
     constructor() {
@@ -26,7 +29,8 @@ export class QuizzClara extends Phaser.Scene {
 
     init() {
         this.Hud = <HudScene>this.scene.get("HudScene");
-        this.Hud.setRemainingTime(Config.QuizzClara.time, false);
+        // this.Hud.setRemainingTime(Config.QuizzClara.time, false);
+        this.Hud.setRemainingTime(3, false);
 	}
 
 	preload() {
@@ -41,33 +45,54 @@ export class QuizzClara extends Phaser.Scene {
         this.StartDialog = new DialogBox(this, this.TextData.Quizz.clara.instruction, false, Anchor.Center, { windowHeight: 410, fontSize: 22 });
         
         this.add.existing(this.StartDialog);
-        let button = this.StartDialog.addArrowButton();
-        button.on('pointerup', this.startQuizz, this);
+        this.Button = this.StartDialog.addArrowButton();
+        this.Button.on('pointerup', this.startQuizz, this);
 
     }
     startQuizz() {
         if (this.GameState != State.Paused){
             return;
         }
-        this.GameState = State.Started;
 
+        this.GameState = State.Started;
+        this.Button.off("pointerup");
         this.StartDialog.destroy();
         this.Hud.startTimer();
-        console.log("faire démarrer le son")
-        let dialogContent = this.cache.json.get('QuizzClara');
-        this.cameras.main.setBackgroundColor("#ffffff");
+        let flash = this.sound.add('ClaraFlash', {
+			mute: false,
+			volume: 1,
+			rate: 1,
+			detune: 0,
+			seek: 0,
+			loop: false,
+			delay: 0
+		});
+        flash.play();
+        this.GameState = State.Started;
+        this.TextDuringFlash = new GameText(this, Config.Game.centerX, Config.Game.centerY, this.TextData.Quizz.clara.duringFlash);
+		this.TextDuringFlash.setOrigin(0.5, 0.5);
+        this.TextDuringFlash.setSize(40);
         
-
-        this.Dialogs = new DialogTree(this, dialogContent, false, Anchor.Down, {windowHeight: 500});
-
-        this.add.existing(this.Dialogs);
-        this.Dialogs.on('destroy', () => {
-            this.scene.start('ClaraConv');
-        });
-
+     
 
 }
     update() {
+        if (this.Hud.getRemainingTime() <= 0 && this.GameEnded == false){
+            console.log("here")
+            this.TextDuringFlash.destroy();
+            
+            this.GameEnded = true;       
+            let dialogContent = this.cache.json.get('QuizzClara');
+            this.cameras.main.setBackgroundColor("#ffffff");
+            
+    
+            this.Dialogs = new DialogTree(this, dialogContent, false, Anchor.Down, {windowHeight: 500});
+    
+            this.add.existing(this.Dialogs);
+            this.Dialogs.on('destroy', () => {
+                this.scene.start('ClaraConv');
+            });     
+        }
 
     }
 }
